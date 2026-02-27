26b5bda317aca5220f1c1150e173bf8ef2507055
xidian12 <xidian@xidian.edu.cn>
2026-02-27 21:51:51 +0800

    init

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# run the script where the setup.py locates with source command
+# e.g. source scripts/docker/maskrcnn_setup.sh maskrcnn_env
+
+env_name=$1
+
+conda create -n ${env_name} python=3.8 -y
+conda activate ${env_name}
+
+# cudatoolkit version should be same as that of pytorch
+# otherwise it will cause error compiling csrc
+conda install gcc=9.5 gxx=9.5 cudatoolkit-dev=11.3 -c conda-forge -y
+conda install pycocotools fvcore iopath ninjas -c conda-forge -y
+conda install pytorch==1.10.0 torchvision==0.11.0 cudatoolkit=11.3 mkl=2024.0 -c pytorch -c conda-forge
+
+python setup.py build develop
+python setup.py clean --all
